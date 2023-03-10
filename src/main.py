560d67e6dc453e13a1c09a8d40087e53a7dfe7cb
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-03-10 09:54:03 +0300

    Initialize headless

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import supervisely as sly
+from supervisely import logger
+
+import src.sly_functions as f
+import src.sly_globals as g
+
+
+logger.info("Application has been started")
+
+################
+# your code here
+################
+
+f.shutdown_app()
---
+++ src/sly_functions.py
@@ -0,0 +1,8 @@
+import supervisely as sly
+
+
+def shutdown_app():
+    try:
+        sly.app.fastapi.shutdown()
+    except KeyboardInterrupt:
+        sly.logger.info("Application shutdown successfully")
