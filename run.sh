78fc8d2ea730add183c6235e3570936e2f1d3bb4
Ars Vladimirov <grokhael@gmail.com>
2023-03-14 09:40:51 +0600

    fix2

--- run.sh
+++ run.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 # supervisely (cli) download --project_id=${PROJECT_ID} --save-dir="/a/b/c" && \
-python3 typer_commands.py download 17732 src/download/ && \
+python3 typer_commands.py download ${PROJECT_ID} src/download/ && \
 
 # tensorboard --logdir src/artefacts --port 8000 && \
 # python3 src/customers_main.py src/download/ds1/img src/artefacts/ && \
 
 # supervisely (cli) upload_directory --tesm_id=${TEAM_ID} --from="/artifacts" --to="/my-training/{TASK_id}{PROJECT_NAME}-{PROJECT_ID}"
-python3 typer_commands.py upload-directory 449 src/download/ds1/img/ /uploaddir && \
+python3 typer_commands.py upload-directory ${TEAM_ID} src/download/ds1/img/ /uploaddir/${TASK_ID}-${PROJECT_ID}/ && \
 # && \
 
 # supervisely (cli) set-task-output-dir ${TASK_ID} "/my-training/{TASK_id}{PROJECT_NAME}-{PROJECT_ID}"
-python3 typer_commands.py set-task-output-dir 28737 /uploaddir
-
+python3 typer_commands.py set-task-output-dir ${TASK_ID} /uploaddir/${TASK_ID}-${PROJECT_ID}/
