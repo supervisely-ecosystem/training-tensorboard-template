a03ff807d3573d764a213174befc082705f88931
Ars Vladimirov <grokhael@gmail.com>
2023-03-23 11:30:49 +0600

    argparse + cleaninf run.sh

--- run.sh
+++ run.sh
@@ -4,47 +4,32 @@ if [ "$ENV" = "development" ]
 then
     source ~/supervisely.env
     source local.env
-fi && \
+fi && 
 
-# INPUT_DIR=
-# OUTPUT_DIR=
+INPUT_DIR=/tmp/training_data/ # training data
+OUTPUT_DIR=$SLY_APP_DATA_DIR # artefacts data
 
-# supervisely download --project-id $PROJECT_ID --dst /tmp/training_data/ && \
-python3 typer_old.py download-project ${PROJECT_ID} /tmp/training_data/ && \
-
-# python3 globals_debug.py && \
-# SYNCED_DIR=`cat synced_dir.txt` && \
-# echo "Synced dir for backup while training: " $SYNCED_DIR && \
+supervisely download --project-id $PROJECT_ID --dst $INPUT_DIR && \
 
 # run tensorboard
-# nohup tensorboard --logdir $SYNCED_DIR --port 8000  --host 0.0.0.0 --reload_multifile=true --load_fast=false --path_prefix=$BASE_URL &> output & sleep 5 && \
+# nohup tensorboard --logdir $OUTPUT_DIR --port 8000  --host 0.0.0.0 --reload_multifile=true --load_fast=false --path_prefix=$BASE_URL &> output & sleep 5 && \
 
-# only debug
-
-# SYNCED_DIR=`cat synced_dir.txt` && \
-# echo "Synced dir for backup while training: " $SYNCED_DIR && \
-
-# SYNCED_DIR=$(supervisely get-synced-dir)  && \
+echo syncdir:
+echo $SLY_APP_DATA_DIR
 
 # training script
-# python3 src/train.py --input-dir /tmp/training_data/ --output-dir $SLY_APP_DATA_DIR && \
+python3 src/train.py --input-dir $INPUT_DIR --output-dir $OUTPUT_DIR  && \
 
 
-# PROJECT_NAME=$(supervisely get-project-name --id $PROJECT_ID --replace-space)  && \
-
+PROJECT_NAME=$(supervisely get-project-name --id $PROJECT_ID --replace-space)  && \
 # upload artefacts
-# supervisely upload --team-id $TEAM_ID --src $SLY_APP_DATA_DIR --dst /my-training/${TASK_ID}-${PROJECT_ID}-${PROJECT_NAME}/ && \
-python3 typer_debug.py upload ${TEAM_ID} /tmp/training_data/ /my-training/${TASK_ID}-${PROJECT_ID}-${PROJECT_NAME}/ && \
+supervisely upload --team-id $TEAM_ID --src $SLY_APP_DATA_DIR --dst /my-training/${TASK_ID}-${PROJECT_ID}-${PROJECT_NAME}/ && \
 
 # WHAT IF FAIL?
 
 # remove useless synced_dir
-
 # supervisely remove-dir --team-id $TEAM_ID --path $SYNCED_DIR 
-# python3 typer_debug.py remove-dir $TEAM_ID $SYNCED_DIR && \
-
 
 # update workspace interface
 
-# supervisely set-task-output-dir --team-id ${TEAM_ID} --task-id ${TASK_ID} --dir /my-training/${TASK_ID}-${PROJECT_ID}-${PROJECT_NAME}/
-# python3 typer_debug.py set-task-output-dir  ${TEAM_ID} ${TASK_ID} /my-training/${TASK_ID}-${PROJECT_ID}-${PROJECT_NAME}/
+supervisely set-task-output-dir --team-id $TEAM_ID --task-id $TASK_ID --dir /my-training/${TASK_ID}-${PROJECT_ID}-${PROJECT_NAME}/
--- src/train.py
+++ src/train.py
@@ -1,25 +1,16 @@
-import supervisely as sly
+import argparse
+import os
 import time
 
-import typer
-import os
-
+import supervisely as sly
 from torch.utils.tensorboard import SummaryWriter
 
-app = typer.Typer()
-
-
-@app.command()
-def train(    
-        input_dir: str = typer.Option(..., "--input-dir", "-i", help="Input directory"),
-        output_dir: str = typer.Option(..., "--output-dir", "-o", help="Output directory"),
-    ):
+def train(input_dir: str, output_dir: str) -> None:
     """
     Gets data from input_dir, trains a model, generates artefacts as output data,
     and logs the training process. Generated artefacts backed up using synced_dir.
     """
-    typer.echo(f"Opening data from {input_dir}...")
-
+    print(f"Opening data from {input_dir}...")
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -27,12 +18,11 @@ def train(
     # Start a TensorBoard writer
     writer = SummaryWriter(output_dir)
 
-    typer.echo("Training model...")
-    typer.echo(f"Generating output artifacts in {output_dir}...")
+    print("Training model...")
+    print(f"Generating output artifacts in {output_dir}...")
 
     n_iter = 10
     progress = sly.Progress(message='Training...', total_cnt=n_iter)
-
 
     for step in range(n_iter):
 
@@ -41,20 +31,26 @@ def train(
 
         # Log the data to TensorBoard
         writer.add_scalar('Loss', loss, step)
-        typer.echo(f"Step {step}, loss={loss:.4f}")
+        print(f"Step [{step}]: loss={loss:.4f}")
 
         file_path = os.path.join(output_dir, f'step_{str(step).zfill(len(str(n_iter)))}.txt')
         
         with open(file_path, 'w') as f:
-            f.write('Step\tLoss\n')
-            f.write(f'{step}\t{loss:.4f}\n')
+            f.write(f"Step [{step}]: loss={loss:.4f}")
 
         progress.iter_done_report()
-               
+
     # Close the TensorBoard writer
     writer.close()
 
-    typer.echo(f"Artefacts generated in {output_dir}!")
+    print(f"Artefacts generated in {output_dir}!")
 
 if __name__ == "__main__":
-    app()
+    parser = argparse.ArgumentParser(description="Training tensorboard template")
+    parser.add_argument("--input-dir", "-i", required=True, help="Input directory")
+    parser.add_argument("--output-dir", "-o", required=True, help="Output directory")
+
+    args = parser.parse_args()
+
+    train(args.input_dir, args.output_dir)
+
