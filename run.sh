f168566cfcb0db8f207c110871cedca39328e0f4
Ars Vladimirov <grokhael@gmail.com>
2023-03-23 21:50:13 +0600

    cleaning shell script

--- run.sh
+++ run.sh
@@ -1,35 +1,36 @@
 # !/bin/bash
-set -e # This will cause the script to exit immediately if any command exits with a non-zero status.
+set -e # This will cause the python script to exit immediately if any command exits with a non-zero status.
 
 if [ "$ENV" = "development" ]
 then
-    source ~/supervisely.env && \
-    source local.env
-fi && \
+    source ~/supervisely.env 
+    source local.env 
+    export SERVER_ADDRESS 
+    export API_TOKEN 
+fi 
+
 
 INPUT_DIR="/tmp/training_data/"     # training data 
-OUTPUT_DIR=$SLY_APP_DATA_DIR  # artefacts data $SLY_APP_DATA_DIR is for synced_data_dir which backups training artefacts
-PROJECT_NAME=$(supervisely get-project-name --id $PROJECT_ID)  
+OUTPUT_DIR=$SLY_APP_DATA_DIR        # artefacts data 
+# Note: variable $SLY_APP_DATA_DIR is for synced_data_dir which backups training artefacts
+PROJECT_NAME=$(supervisely get-project-name --id $PROJECT_ID) 
 
-# echo $ENV && \
-# echo $INPUT_DIR && \
-# echo $OUTPUT_DIR && \
-# echo $PROJECT_NAME && \
 
 # download project
-supervisely download --project-id $PROJECT_ID --dst $INPUT_DIR && \
+supervisely download --project-id $PROJECT_ID --dst $INPUT_DIR 
+
 
 # run tensorboard
-nohup tensorboard --logdir $OUTPUT_DIR --port 8000  --host 0.0.0.0 --reload_multifile=true --load_fast=false --path_prefix=$BASE_URL &> output & sleep 5 && \
+nohup tensorboard --logdir $OUTPUT_DIR --port 8000  --host 0.0.0.0 --reload_multifile=true --load_fast=false --path_prefix=$BASE_URL &> output & sleep 5 
+
 
 # training script
-python3 src/train.py --input-dir $INPUT_DIR --output-dir $OUTPUT_DIR  && \
+python3 src/train.py --input-dir $INPUT_DIR --output-dir $OUTPUT_DIR  
 
 
 # upload artefacts
-supervisely upload --team-id $TEAM_ID --src $OUTPUT_DIR --dst "/my-training/$TASK_ID-$PROJECT_ID-$PROJECT_NAME/" && \
-echo Removing output directory $OUTPUT_DIR  && \
-supervisely remove-dir --team-id $TEAM_ID --path $OUTPUT_DIR && \
+supervisely upload --team-id $TEAM_ID --src $OUTPUT_DIR --dst "/my-training/$TASK_ID-$PROJECT_ID-$PROJECT_NAME/" 
+supervisely remove-dir --team-id $TEAM_ID --path $OUTPUT_DIR 
 
 
 # update workspace interface
