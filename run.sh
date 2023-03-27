e37d6b1b239df0243010ed5704f136e47360c3c4
Maxim Kolomeychenko <maxim@supervise.ly>
2023-03-27 22:25:40 +0300

    format run.sh

--- run.sh
+++ run.sh
@@ -9,24 +9,19 @@ then
     export API_TOKEN
 fi 
 
-
 INPUT_DIR="/tmp/training_data/"     # training data 
 OUTPUT_DIR=$SLY_APP_DATA_DIR        # artefacts data 
 # Note: variable $SLY_APP_DATA_DIR is for synced_data_dir which mirrors artefacts data on teamfiles
 PROJECT_NAME=$(supervisely project get-name -id $PROJECT_ID)
 
-
 # download project 
 supervisely project download -id $PROJECT_ID --dst $INPUT_DIR
-
 
 # run tensorboard
 nohup tensorboard --logdir $OUTPUT_DIR --port 8000  --host 0.0.0.0 --reload_multifile=true --load_fast=false --path_prefix=$BASE_URL &> output & sleep 5 
 
-
 # training script
 python3 src/train.py --input-dir $INPUT_DIR --output-dir $OUTPUT_DIR  
-
 
 # upload artefacts
 supervisely teamfiles upload -id $TEAM_ID --src $OUTPUT_DIR --dst "/my-training/$TASK_ID-$PROJECT_ID-$PROJECT_NAME/" 
