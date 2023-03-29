8b9f39a0687abcfbd242fa9677e78f87d4262c54
Ars Vladimirov <grokhael@gmail.com>
2023-03-29 14:16:13 +0600

    change run.sh to test upload progress

--- run.sh
+++ run.sh
@@ -21,13 +21,13 @@ supervisely project download -id $PROJECT_ID --dst $INPUT_DIR
 
 
 # run tensorboard
-nohup tensorboard --logdir $OUTPUT_DIR --port 8000  --host 0.0.0.0 --reload_multifile=true --load_fast=false --path_prefix=$BASE_URL &> output & sleep 5 
+# nohup tensorboard --logdir $OUTPUT_DIR --port 8000  --host 0.0.0.0 --reload_multifile=true --load_fast=false --path_prefix=$BASE_URL &> output & sleep 5 
 
 # training script
-python3 src/train.py --input-dir $INPUT_DIR --output-dir $OUTPUT_DIR  
+# python3 src/train.py --input-dir $INPUT_DIR --output-dir $OUTPUT_DIR  
 
 # upload artefacts
-supervisely teamfiles upload -id $TEAM_ID --src $OUTPUT_DIR --dst "/my-training/$TASK_ID-$PROJECT_ID-$PROJECT_NAME/" 
+supervisely teamfiles upload -id $TEAM_ID --src $INPUT_DIR --dst "/my-training/$TASK_ID-$PROJECT_ID-$PROJECT_NAME/" 
 supervisely task set-output-dir -id $TASK_ID --team-id $TEAM_ID  --dir "/my-training/$TASK_ID-$PROJECT_ID-$PROJECT_NAME/"
 
 
