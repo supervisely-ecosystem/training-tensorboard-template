3e38c949d2852e62c726abdb402e31f81c15f4f0
Ars Vladimirov <grokhael@gmail.com>
2023-03-31 20:08:10 +0600

    reading from output

--- run.sh
+++ run.sh
@@ -26,11 +26,23 @@ supervisely project download -id $PROJECT_ID --dst $INPUT_DIR
 # upload artefacts
 supervisely teamfiles upload -id $TEAM_ID --src $INPUT_DIR --dst "/my-training/$TASK_ID-$PROJECT_ID-$PROJECT_NAME/" 
 
-if [ "$ENV" != "development" ]
-then
-    supervisely task set-output-dir -id $TASK_ID --team-id $TEAM_ID  --dir "/my-training/$TASK_ID-$PROJECT_ID-$PROJECT_NAME/"
-fi 
+
+# output=$(supervisely teamfiles upload -id $TEAM_ID --src $INPUT_DIR --dst "/my-training/$TASK_ID-$PROJECT_ID-$PROJECT_NAME/" | tee /dev/tty)
+
+
+# directory=$(echo $output | grep -o "Local directory was sucessfully uploaded to Team files directory: '[^']*'")
+# TEAMFILES_DIR=$(echo $directory | grep -o "'[^']*'" | sed "s/'//g")
+
+
+# if [ "$TEAMFILES_DIR" != "/my-training/$TASK_ID-$PROJECT_ID-$PROJECT_NAME/" ]
+# then
+#     echo "local and remote directories not matching!" #do your code here
+#     echo "Actual Team files directory: $TEAMFILES_DIR"
+# fi
+
+supervisely task set-output-dir -id $TASK_ID --team-id $TEAM_ID  --dir "/my-training/$TASK_ID-$PROJECT_ID-$PROJECT_NAME/"
+
 
 # cleaning the space on agent
-echo "Deleting $OUTPUT_DIR contents"
+echo "Deleting '$OUTPUT_DIR' contents"
 rm -rf $OUTPUT_DIR/*
