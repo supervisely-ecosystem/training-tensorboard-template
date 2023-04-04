4299b60fba3af9a1b95316dd93b6852d48a314f5
Ars Vladimirov <grokhael@gmail.com>
2023-04-04 16:41:57 +0600

    test on instance

--- run.sh
+++ run.sh
@@ -27,10 +27,10 @@ supervisely project download -id $PROJECT_ID --dst $INPUT_DIR
 supervisely teamfiles upload -id $TEAM_ID --src $INPUT_DIR --dst "/my-training/$TASK_ID-$PROJECT_ID-$PROJECT_NAME/" 
 
 
-# supervisely task set-output-dir -id $TASK_ID --team-id $TEAM_ID  --dir "/my-training/$TASK_ID-$PROJECT_ID-$PROJECT_NAME/"
+supervisely task set-output-dir -id $TASK_ID --team-id $TEAM_ID  --dir "/my-training/$TASK_ID-$PROJECT_ID-$PROJECT_NAME/"
 
 # supervisely task set-output-dir -id $TASK_ID --team-id $TEAM_ID  --dir  "/empty/"
-supervisely task set-output-dir --dir  "/my-training/$TASK_ID-$PROJECT_ID-$PROJECT_NAME/"
+# supervisely task set-output-dir --dir  "/my-training/$TASK_ID-$PROJECT_ID-$PROJECT_NAME/"
 
 
 # cleaning the space on agent
