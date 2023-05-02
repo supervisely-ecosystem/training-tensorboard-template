242b48b28060dd057935c62db7ddc6cca587ce8e
Ars Vladimirov <grokhael@gmail.com>
2023-05-02 22:37:22 +0600

    fix str

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@ OUTPUT_DIR=$SLY_APP_DATA_DIR        # current artefacts data
 # Note: variable $SLY_APP_DATA_DIR is for synced_data_dir which mirrors artefacts data on teamfiles
 PROJECT_NAME=$(supervisely project get-name -id $PROJECT_ID)
 HISTORY_DIR="/my-training/"        # history artefacts data in Team files
-DST_DIR="/my-training/$TASK_ID-$PROJECT_ID-$PROJECT_NAME/" # destination directory in Team files
+# DST_DIR="/my-training/$TASK_ID-$PROJECT_ID-$PROJECT_NAME/" # destination directory in Team files
 
 # download project 
 supervisely project download -id $PROJECT_ID --dst $INPUT_DIR
@@ -30,11 +30,11 @@ nohup tensorboard --logdir $OUTPUT_DIR --port 8000 --host 0.0.0.0 --reload_multi
 python3 src/train.py --input-dir $INPUT_DIR --output-dir $OUTPUT_DIR  
 
 # upload artefacts
-supervisely teamfiles upload -id $TEAM_ID --src $OUTPUT_DIR --dst "$DST_DIR"
+supervisely teamfiles upload -id $TEAM_ID --src $OUTPUT_DIR --dst "/my-training/$TASK_ID-$PROJECT_ID-$PROJECT_NAME/"
 
 if [ "$ENV" != "development" ]
 then
-    supervisely task set-output-dir -id $TASK_ID --team-id $TEAM_ID  --dir "$DST_DIR"
+    supervisely task set-output-dir -id $TASK_ID --team-id $TEAM_ID  --dir "/my-training/$TASK_ID-$PROJECT_ID-$PROJECT_NAME/"
 fi 
 
 # cleaning the space on agent
