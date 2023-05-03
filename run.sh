a7e5e7a752357b5c065ecc5fc99a4dc4f79efa41
Ars Vladimirov <grokhael@gmail.com>
2023-05-03 12:41:51 +0600

    change names

--- run.sh
+++ run.sh
@@ -9,25 +9,25 @@ then
     export API_TOKEN
 fi 
 
-INPUT_DIR="/tmp/training_data/"                         # training data
-OUTPUT_DIR_LOCAL="$SLY_APP_DATA_DIR/output"       # current artefacts data
+INPUT_DIR_LOCAL="/tmp/training_data/"                   # local training data
+OUTPUT_DIR_LOCAL="$SLY_APP_DATA_DIR/output"             # local output artefacts data
 # Note: variable $SLY_APP_DATA_DIR is for synced_data_dir which mirrors artefacts data on teamfiles
 PROJECT_NAME=$(supervisely project get-name -id $PROJECT_ID)
-HISTORY_DIR="/my-training/"                             # history artefacts data in Team files
-HISTORY_DIR_LOCAL="$SLY_APP_DATA_DIR/history"           # current history data
-DST_DIR="/my-training/$TASK_ID-$PROJECT_ID-$PROJECT_NAME/" # destination directory in Team files
+HISTORY_DIR="/my-training/"                             # teamfiles history artefacts data
+HISTORY_DIR_LOCAL="$SLY_APP_DATA_DIR/history"           # local history artefacts data
+DST_DIR="/my-training/$TASK_ID-$PROJECT_ID-$PROJECT_NAME/" # destination directory in Team files for output artefacts data
 
 # download project 
-supervisely project download -id $PROJECT_ID --dst $INPUT_DIR
+supervisely project download -id $PROJECT_ID --dst $INPUT_DIR_LOCAL
 
 # download history artefacts
 supervisely teamfiles download -id $TEAM_ID --src "$HISTORY_DIR" --dst "$HISTORY_DIR_LOCAL" --filter ".tfevents." -i
 
 # run tensorboard
-nohup tensorboard --logdir_spec Current:"$OUTPUT_DIR_LOCAL",History:"$HISTORY_DIR_LOCAL" --port 8000 --host 0.0.0.0 --reload_multifile=true --load_fast=false --path_prefix=$BASE_URL &> output & sleep 5 
+nohup tensorboard --logdir_spec output:"$OUTPUT_DIR_LOCAL",history:"$HISTORY_DIR_LOCAL" --port 8000 --host 0.0.0.0 --reload_multifile=true --load_fast=false --path_prefix=$BASE_URL &> output & sleep 5 
 
 # training script
-python3 src/train.py --input-dir "$INPUT_DIR" --output-dir "$OUTPUT_DIR_LOCAL"  
+python3 src/train.py --input-dir "$INPUT_DIR_LOCAL" --output-dir "$OUTPUT_DIR_LOCAL"
 
 # upload artefacts
 supervisely teamfiles upload -id $TEAM_ID --src "$OUTPUT_DIR_LOCAL" --dst "$DST_DIR"
